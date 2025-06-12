caef5bc676e0b35df029d27a5971a383173f078b
unknown <sabeebr97@gmail.com>
2025-06-12 04:04:18 -0700

    updated config

--- puppeteer.config.js
+++ puppeteer.config.js
@@ -2,6 +2,9 @@ import { join } from 'path';
 
 const puppeteerConfig = {
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  chrome: {
+    skipDownload: false,
+  },
 };
 
 export default puppeteerConfig;
