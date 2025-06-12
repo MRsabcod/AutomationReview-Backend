18d834fb4350e38ad6f8603fefe8b5991ca4f20a
unknown <sabeebr97@gmail.com>
2025-06-12 07:52:18 -0700

    packages updated 2

--- api/index.js
+++ api/index.js
@@ -7,13 +7,12 @@ import cookieParser from 'cookie-parser';
 import 'dotenv/config'
 
 import router from '../routes/index.js';
-import { connectDB } from '../db/index.js';
-import multer from 'multer';
+
 import cors from 'cors'
 // import {fetchPerformanceData} from './utils/googleBusinessPerfomance.js';
 
 const app = express();
-const upload = multer({ storage: multer.memoryStorage() });
+
 
 
 app.use(express.json());
@@ -29,12 +28,10 @@ app.use('/', router);
 
 
 // Error handler
-connectDB()
-.then(() => {
+
 const server=app.listen(process.env.PORT || 8000, () => {
   console.log(`⚙️ Server is running at port : ${process.env.PORT|| server.address().port}`);
   // fetchPerformanceData()
-})
 })
 // Export the app as a module
 export default app;
